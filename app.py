4f7d4f6bb0a45e2a39aa64c53a68f35cc51aa77a
«Eugene <e.pavlov98@mail.ru>
2020-11-01 01:11:38 +0700

    work with templates was studied

--- app.py
+++ app.py
@@ -12,7 +12,26 @@ def hello_world():
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # выдуманный пользователь
-    return render_template("index.html", title='Home', user=user)
+    return render_template("index.html",  user=user)
+
+
+@app.route('/posts')
+def posts():
+    user = {'nickname': 'Miguel'}  # выдуманный пользователь
+    posts = [  # список выдуманных постов
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("posts.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 if __name__ == '__main__':
